08ca506ed538174eb1fe12506fa15bed092ab18f
aabhinaav <aabhinaavgarg@gmail.com>
2024-01-19 20:28:13 +0530

    configurations added to applicatin.yml dependencies -> web, mysql, jpa, lombok, graphql

---
+++ src/test/java/com/graphql/learn/Spring_GraphQL/SpringGraphQlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.graphql.learn.Spring_GraphQL;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringGraphQlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
